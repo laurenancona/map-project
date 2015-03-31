5267953674d23bac5576a92d0495ff22035fc805
Lauren Ancona <lala.ancona@gmail.com>
2015-03-31 08:44:09 -0400

    stupid-fast blueprint wrapper

--- js/app.js
+++
@@ -1,9 +0,0 @@
-/*globals L, cartodb: true */
-
-$(function(){
-  var map = L.map('map').setView([42.42, -83.02 ], 13);
-
-  var baseLayer = L.tileLayer('http://a.tiles.mapbox.com/v3/matth.map-zmpggdzn/{z}/{x}/{y}.png');
-  map.addLayer(baseLayer);
-
-});
